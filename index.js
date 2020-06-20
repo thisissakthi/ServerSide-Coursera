f64c1da01928707d601e3cb733b640eb8e020829
Sakthi Prasath <mspprasath1133@gmail.com>
2020-06-20 11:12:39 +0530

    errors

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var rect = require('./rectangle');
-
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
-    if (l <= 0 || b <= 0) {
-        console.log("Rectangle dimensions should be greater than zero:  l = "
-               + l + ",  and b = " + b);
-    }
+    rect(l,b,(err,rectangle)=>{
+      if(err)
+      {
+        console.log("ERROR: ",err.message);
+      }
     else {
-	    console.log("The area of the rectangle is " + rect.area(l,b));
-	    console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
+	    console.log("The area of the rectangle is " + rectangle.area(l,b));
+	    console.log("The perimeter of the rectangle is " + rectangle.perimeter(l,b));
     }
-}
+  });
+};
 
 solveRect(2,4);
 solveRect(3,5);
