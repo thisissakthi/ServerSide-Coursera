25f54ea4b0c33792ea08e77896cffaa024bdb1c7
Sakthi Prasath <mspprasath1133@gmail.com>
2020-06-20 23:08:16 +0530

    morgan

--- Express/index.js
+++ Express/index.js
@@ -4,7 +4,9 @@ const http=require(`http`);
 const hostname=`localhost`;
 const port=3000;
 const app=express();
-
+const morgan=require(`morgan`);
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
 app.use((req,res, next)=>{
   console.log(req.headers);
   req.statuscode=200;
