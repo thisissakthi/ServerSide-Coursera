ba6ad55772c38bce13b088edb63fa964704b13ec
Sakthi Prasath <mspprasath1133@gmail.com>
2020-06-21 12:14:06 +0530

    express added

--- Express/index.js
+++ Express/index.js
@@ -1,10 +1,55 @@
 const express=require(`express`);
 const http=require(`http`);
-
+const bodyParser =require(`body-parser`);
 const hostname=`localhost`;
 const port=3000;
 const app=express();
 const morgan=require(`morgan`);
+app.use(bodyParser.json());
+app.all(`/dishes`,(req,res,next)=>
+{
+  res.statusCode=200;
+  res.setHeader(`Content-Type`,`text/plain`);
+  next();
+});
+app.get(`/dishes`,(req,res,next)=>
+{
+  res.end(`will send all the dishes to you!`);
+});
+app.post(`/dishes`,(req,res,next)=>
+{
+  res.end(`Will add the dish: `+req.body.name+` with details` +req.body.description);
+});
+app.put(`/dishes`,(req,res,next)=>
+{
+  res.statusCode=403;
+  res.end('PUT operation not supported on /dishes');
+});
+app.delete(`/dishes`,(req,res,next)=>
+{
+  res.end(`Deleting all Dishes`);
+});
+
+app.get(`/dishes/:dishId`,(req,res,next)=>
+{
+  res.end(`will send all the dishes to you!`);
+});
+app.post(`/dishes/:dishId`,(req,res,next)=>
+{
+  res.statusCode=403;
+  res.end(`WPost `+ req.params.dishId);
+});
+app.put(`/dishes/:dishId`,(req,res,next)=>
+{
+  res.end('PUT on /dishes '+req.params.dishId +`\n`);
+  res.end(`Will update `+req.body.name+` discription `+req.body.description);
+});
+app.delete(`/dishes/:dishId`,(req,res,next)=>
+{
+  res.end(`Deleting all Dishes`+req.params.dishId);
+});
+
+
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use((req,res, next)=>{
